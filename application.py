ed28e43714364b1595c243c5c438fb55f66250bd
Gary Su <garysu@Garys-Laptop.local>
2023-07-11 23:26:38 -0400

    fix NN, start simple UI

--- application.py
+++ application.py
@@ -3,9 +3,9 @@ from torch import nn
 import torch.nn.functional as func
 import streamlit as st
 import numpy as np
-from scipy.ndimage.interpolation import zoom
+# from scipy.ndimage.interpolation import zoom
 
-class NN(nn.module):
+class NN(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
@@ -21,3 +21,18 @@ class NN(nn.module):
     
     def forward(self, x):
         return self.model(x)
+
+def main():
+    st.title("MNIST Digit Predictor")
+    l_col, r_col = st.columns(2)
+
+    with l_col:
+        st.header("Draw a digit from 1-9")
+
+    with r_col:
+        st.header("Predicted:")
+        # change below
+        st.title("Something...")
+
+if __name__ == "__main__":
+    main()
